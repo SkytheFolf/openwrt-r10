726f6de748212f0f0b21f9141172d8e597c140cd
Skylar Folf <informationconflicts@protonmail.com>
2026-09-22 12:07:55 -0400

    qualcommbe: add support for Alta Labs Route10
    Add initial support for the IPQ9574-based Alta Labs Route10.
    
    Specifications:
     - SoC: Qualcomm IPQ9574 (quad-core Cortex-A73)
     - RAM: 1 GB
     - Storage: eMMC
     - Ethernet: QCA8084 quad PHY (3x LAN + 1x WAN, 2.5G)
     - SFP: 2x 10G cages on UNIPHY1 (wan2) and UNIPHY2 (lan4)
     - LEDs: 4x SFP link LEDs on GPIO
    
    This covers building the board images and booting the initramfs image
    over TFTP from the U-Boot console:
    
      setenv serverip <tftp-server>
      tftpboot 0x50000000 openwrt-qualcommbe-ipq95xx-alta_route10-initramfs-uImage.itb
      bootm 0x50000000
    
    Installation to eMMC and sysupgrade are not wired up yet and will
    follow separately, as will software control of the RJ45 port LEDs,
    which needs LED support in the QCA8084 PHY driver.
    
    Co-developed-by: gchahcg <7381837@proton.me>
    Signed-off-by: gchahcg <7381837@proton.me>
    Signed-off-by: Skylar Folf <informationconflicts@protonmail.com>

--- target/linux/qualcommbe/image/ipq95xx.mk
+++ target/linux/qualcommbe/image/ipq95xx.mk
@@ -14,6 +14,15 @@ define Device/8devices_kiwi-dvk
 endef
 TARGET_DEVICES += 8devices_kiwi-dvk
 
+define Device/alta_route10
+	$(call Device/FitImage)
+	DEVICE_VENDOR := Alta Labs
+	DEVICE_MODEL := Route10
+	SOC := ipq9574
+	DEVICE_PACKAGES := kmod-sfp
+endef
+TARGET_DEVICES += alta_route10
+
 define Device/askey_sbe1v1k
 	$(call Device/FitImage)
 	$(call Device/EmmcImage)
